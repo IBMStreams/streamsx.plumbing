8942b767a5dd21c963f381c054360f1e7b049794
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-03-24 10:51:50 -0700

    Fix indentation to spaces

--- com.ibm.streamsx.plumbing/impl/java/src/com/ibm/streamsx/plumbing/leader/LeadershipElection.java
+++ com.ibm.streamsx.plumbing/impl/java/src/com/ibm/streamsx/plumbing/leader/LeadershipElection.java
@@ -36,142 +36,141 @@ import com.ibm.streams.operator.model.PrimitiveOperator;
  * election between itself and other instances of {@code LeadershipElection}
  * with the same group identifier.
  */
-@PrimitiveOperator(description=LeadershipElection.OP_DESC)
+@PrimitiveOperator(description = LeadershipElection.OP_DESC)
 @Libraries("opt/apache-curator-2.7.1/*")
 public class LeadershipElection extends AbstractOperator implements Controllable {
-	
-	public static final String OP_DESC = "Performs a leadership election between other operator invocations with the same `group`. "
-			+ "At any time only one invocation is the leader, should a leader have a failure a new leader will be elected. "
-			+ "Invocations must be exlocated into different PEs."
-			+ "\\n\\n"
-			+ "The operator requires the application include an invocation of the `JobControlPlane` operator."
-			+ "\\n\\n"
-			+ "Leadership election uses Apache Curator's leadership latch recipe against the Streams instance Zookeeper service."
-			+ "The Zookeeper root node for the recipe is a sub-node `group` within job's storage node "
-			+ "(`com.ibm.streams.management.job.JobMXBean.retrieveZooKeeperStorageNode()`).";
 
-	/**
-	 * Group name for leadership election.
-	 */
-	private String group;
+    public static final String OP_DESC = "Performs a leadership election between other operator invocations with the same `group`. "
+            + "At any time only one invocation is the leader, should a leader have a failure a new leader will be elected. "
+            + "Invocations must be exlocated into different PEs." + "\\n\\n"
+            + "The operator requires the application include an invocation of the `JobControlPlane` operator."
+            + "\\n\\n"
+            + "Leadership election uses Apache Curator's leadership latch recipe against the Streams instance Zookeeper service."
+            + "The Zookeeper root node for the recipe is a sub-node `group` within job's storage node "
+            + "(`com.ibm.streams.management.job.JobMXBean.retrieveZooKeeperStorageNode()`).";
 
-	/**
-	 * Metric indicating if this operator is the leader.
-	 */
-	private Metric isLeader;
+    /**
+     * Group name for leadership election.
+     */
+    private String group;
 
-	private CuratorFramework client;
-	private LeaderLatch leaderLatch;
+    /**
+     * Metric indicating if this operator is the leader.
+     */
+    private Metric isLeader;
 
-	private String id;
+    private CuratorFramework client;
+    private LeaderLatch leaderLatch;
 
-	private static final Logger trace = Logger.getLogger("com.ibm.streamsx.plumbing");
+    private String id;
 
-	@Override
-	public void initialize(OperatorContext context) throws Exception {
-		super.initialize(context);
-		
-		id = "PE_" + context.getPE().getPEId().toString();
+    private static final Logger trace = Logger.getLogger("com.ibm.streamsx.plumbing");
 
-		String connString = System.getenv("STREAMS_ZKCONNECT");
-		client = CuratorFrameworkFactory.newClient(connString,
-				new ExponentialBackoffRetry(1000, Integer.MAX_VALUE, 5000));
+    @Override
+    public void initialize(OperatorContext context) throws Exception {
+        super.initialize(context);
 
-		client.start();
-		client.getZookeeperClient().blockUntilConnectedOrTimedOut();
+        id = "PE_" + context.getPE().getPEId().toString();
 
-		trace.fine("Connected to Zookeeper using Curator");
-		
-		// Attach as soon as possible, to avoid any delays during allPortsReady.
-		getControlPlaneContext().connect(this);
-	}
+        String connString = System.getenv("STREAMS_ZKCONNECT");
+        client = CuratorFrameworkFactory.newClient(connString,
+                new ExponentialBackoffRetry(1000, Integer.MAX_VALUE, 5000));
 
-	@Override
-	public void shutdown() throws Exception {
-		if (leaderLatch != null) {
-			leaderLatch.close();
-			leaderLatch = null;
-		}
+        client.start();
+        client.getZookeeperClient().blockUntilConnectedOrTimedOut();
 
-		if (client != null) {
-			client.close();
-			client = null;
-		}
-		super.shutdown();
-	}
+        trace.fine("Connected to Zookeeper using Curator");
 
-	@Override
-	public void allPortsReady() {
-	}
+        // Attach as soon as possible, to avoid any delays during allPortsReady.
+        getControlPlaneContext().connect(this);
+    }
 
-	@Parameter(description="Leadership group name. All instances of this operator within the same job and having the same `group`"
-			+ " are participants in a single leadership election. Group name must not start with a slash (`/`).")
-	public void setGroup(String group) {
-		this.group = group;
-	}
+    @Override
+    public void shutdown() throws Exception {
+        if (leaderLatch != null) {
+            leaderLatch.close();
+            leaderLatch = null;
+        }
 
-	public String getGroup() {
-		return group;
-	}
+        if (client != null) {
+            client.close();
+            client = null;
+        }
+        super.shutdown();
+    }
 
-	protected Metric getIsLeader() {
-		return isLeader;
-	}
+    @Override
+    public void allPortsReady() {
+    }
 
-	@CustomMetric(kind = Kind.GAUGE, description="Is this invocation the leader. 1 this operator invocation is the leader for the group, 0 is it not the leader.")
-	public void setIsLeader(Metric isLeader) {
-		this.isLeader = isLeader;
-	}
+    @Parameter(description = "Leadership group name. All instances of this operator within the same job and having the same `group`"
+            + " are participants in a single leadership election. Group name must not start with a slash (`/`).")
+    public void setGroup(String group) {
+        this.group = group;
+    }
 
-	@Override
-	public void event(MBeanServerConnection jcp, OperatorContext context, EventType eventType) {
-	}
+    public String getGroup() {
+        return group;
+    }
 
-	@Override
-	public boolean isApplicable(OperatorContext context) {
-		return true;
-	}
+    protected Metric getIsLeader() {
+        return isLeader;
+    }
 
-	@Override
-	public void setup(MBeanServerConnection jcp, OperatorContext context) throws InstanceNotFoundException, Exception {
-		
-		synchronized (this) {
-			if (leaderLatch != null)
-				return;
-		}
+    @CustomMetric(kind = Kind.GAUGE, description = "Is this invocation the leader. 1 this operator invocation is the leader for the group, 0 is it not the leader.")
+    public void setIsLeader(Metric isLeader) {
+        this.isLeader = isLeader;
+    }
 
-		String path = (String) jcp.invoke(ControlPlaneManagement.JOB_NAME, "retrieveZooKeeperStorageNode", null, null);
+    @Override
+    public void event(MBeanServerConnection jcp, OperatorContext context, EventType eventType) {
+    }
 
-		trace.fine("Zookeeper path for this job:" + path);
+    @Override
+    public boolean isApplicable(OperatorContext context) {
+        return true;
+    }
 
-		synchronized (this) {
-		leaderLatch = new LeaderLatch(client, path + "/" + getGroup(), id);
+    @Override
+    public void setup(MBeanServerConnection jcp, OperatorContext context) throws InstanceNotFoundException, Exception {
 
-		leaderLatch.addListener(new LeaderLatchListener() {
+        synchronized (this) {
+            if (leaderLatch != null)
+                return;
+        }
 
-			@Override
-			public void notLeader() {
-				// Set the metric
-				getIsLeader().setValue(0);
-				if (trace.isLoggable(TraceLevel.INFO))
-				    trace.info("Group:" + getGroup() + " id:" + id + " lost the leader");
-			}
+        String path = (String) jcp.invoke(ControlPlaneManagement.JOB_NAME, "retrieveZooKeeperStorageNode", null, null);
 
-			@Override
-			public void isLeader() {
-				// Set the metric
-				getIsLeader().setValue(1);
-				if (trace.isLoggable(TraceLevel.INFO))
-				    trace.info("Group:" + getGroup() + " id:" + id + " became the leader");
+        trace.fine("Zookeeper path for this job:" + path);
 
-			}
-		}, Executors.newFixedThreadPool(1, context.getThreadFactory()));
-		}
-		
-		trace.fine("Starting leadership latch for id: " + id);
-		leaderLatch.start();
-		trace.fine("Started leadership latch for id: " + id);
+        synchronized (this) {
+            leaderLatch = new LeaderLatch(client, path + "/" + getGroup(), id);
 
-	}
+            leaderLatch.addListener(new LeaderLatchListener() {
+
+                @Override
+                public void notLeader() {
+                    // Set the metric
+                    getIsLeader().setValue(0);
+                    if (trace.isLoggable(TraceLevel.INFO))
+                        trace.info("Group:" + getGroup() + " id:" + id + " lost the leader");
+                }
+
+                @Override
+                public void isLeader() {
+                    // Set the metric
+                    getIsLeader().setValue(1);
+                    if (trace.isLoggable(TraceLevel.INFO))
+                        trace.info("Group:" + getGroup() + " id:" + id + " became the leader");
+
+                }
+            }, Executors.newFixedThreadPool(1, context.getThreadFactory()));
+        }
+
+        trace.fine("Starting leadership latch for id: " + id);
+        leaderLatch.start();
+        trace.fine("Started leadership latch for id: " + id);
+
+    }
 
 }
